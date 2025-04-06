e38f05ebf698ee84601ceb688792d6ee879db987
Eliott <yot360@users.noreply.github.com>
2025-04-06 21:17:44 +0000

    Added colors

---
+++ src/Colors.h
@@ -0,0 +1,16 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RESET   "\033[0m"
+
+// Bright Colors
+#define BRIGHT_BLACK   "\033[90m"
+#define BRIGHT_RED     "\033[91m"
+#define BRIGHT_GREEN   "\033[92m"
+#define BRIGHT_YELLOW  "\033[93m"
+#define BRIGHT_BLUE    "\033[94m"
+#define BRIGHT_MAGENTA "\033[95m"
+#define BRIGHT_CYAN    "\033[38;2;63;166;204m"
+#define BRIGHT_WHITE   "\033[97m"
+
+#endif /* COLORS_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "SystemInfo.h"
+#include "Colors.h"
+
 int main(int argc, char *argv[])
 {
 	const CPUInfo cpuinfo = SystemInfo::getCPUInfo();
@@ -11,14 +13,14 @@ int main(int argc, char *argv[])
 	const std::string distro = SystemInfo::getDistro();
 	const std::string kernel = SystemInfo::getKernel();
 
-	std::cout << "╭──[" << hostinfo.username << "@" << hostinfo.hostname << "]──c" << std::endl;
-	std::cout << "│     .--.           " << distro << std::endl;
-	std::cout << "│    |o_o |        󱙤  " << kernel << std::endl;
-	std::cout << "│    |:_/ |          " << shell << std::endl;
-	std::cout << "│   //   \\ \\         " << cpuinfo.model_name << std::endl;
-	std::cout << "│  (|     | )        " << gpu << std::endl;
-	std::cout << "│ /'\\_   _/`\\        " << raminfo.used << "MB/" << raminfo.total << "MB (" << raminfo.percentage << "%)" << std::endl;
-	std::cout << "│ \\___)=(___/        " << uptime.hours << " hours and " << uptime.minutes << " minutes" << std::endl;
+	std::cout << "╭──[" << BRIGHT_RED << hostinfo.username << RESET << "@" << BRIGHT_RED << hostinfo.hostname << RESET << "]──c" << std::endl;
+	std::cout << "│" << BRIGHT_YELLOW << "     .--.         "<< BRIGHT_CYAN << "  " << RESET << distro << std::endl;
+	std::cout << "│" << BRIGHT_YELLOW << "    |o_o |        "<< BRIGHT_CYAN << "󱙤  " << RESET << kernel << std::endl;
+	std::cout << "│" << BRIGHT_YELLOW << "    |:_/ |        "<< BRIGHT_CYAN << "  " << RESET << shell << std::endl;
+	std::cout << "│" << BRIGHT_YELLOW << "   //   \\ \\       "<< BRIGHT_CYAN << "  " << RESET << cpuinfo.model_name << std::endl;
+	std::cout << "│" << BRIGHT_YELLOW << "  (|     | )      "<< BRIGHT_CYAN << "  " << RESET << gpu << std::endl;
+	std::cout << "│" << BRIGHT_YELLOW << " /'\\_   _/`\\      "<< BRIGHT_CYAN << "  " << RESET << raminfo.used << "MB/" << raminfo.total << "MB (" << raminfo.percentage << "%)" << std::endl;
+	std::cout << "│" << BRIGHT_YELLOW << " \\___)=(___/      "<< BRIGHT_CYAN << "  " << RESET << uptime.hours << " hours and " << uptime.minutes << " minutes" << std::endl;
 	std::cout << "╰──────────────────────────c" << std::endl;
 	return 0;
 }
